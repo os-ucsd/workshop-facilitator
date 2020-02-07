206cefb582d39efa193e92b1ddd8f9b7e8997c39
Chau Vu <chv004@ucsd.edu>
2020-02-06 18:14:50 -0800

    created Host component

--- workshop-facilitator/src/App.js
+++ workshop-facilitator/src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
+import {BrowserRouter as Router} from 'react-router';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Route exact path="/host" component={Host}/>
+      </Router>
     </div>
   );
 }
