9aba4a718d0409ac48f89e46741d1e603609d700
blau0123 <brendanwlau@gmail.com>
2020-02-06 17:48:59 -0800

    added create react app

---
+++ test.js
@@ -0,0 +1,26 @@
+class BtnExample extends React.Component{
+    constructor(){
+        super();
+        //this.btnClick = this.btnClick.bind(this);
+        this.state = {asdf: "asdf"}
+    }
+    
+    /*
+    btnClick(){
+        alert("you clicked me");
+        this.state.asdf    
+    }
+    */
+
+    btnClick = () => {
+        alert("you clicked me");
+        this.state.asf
+    }
+
+    render(){
+        return(
+            <button onClick={this.btnClick}>Click</button>
+        );
+    }
+}
+
