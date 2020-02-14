936c67692c76cee1ec4aaa41b7127a93264a695b
blau0123 <brendanwlau@gmail.com>
2020-02-13 19:18:01 -0800

    connected to mongodb, created skeleton api routes, edited utils.js to support multiple function exports

--- workshop-facilitator/src/utils.js
+++ workshop-facilitator/src/utils.js
@@ -1,8 +1,22 @@
 /*
-Returns a random four digit number to be used as the
+@params None
+@desc Calculates a random four digit number to be used as the
 code to join a workshop room
 Range: [1000, 10000)
+@return int roomCode
 */
-export default function generateRandomCode(){
+export function generateRandomCode(){
     return Math.floor(1000 + Math.random() * 9000);
 }
+
+/*
+@params String file/URL name
+@desc Decides what type of file or URL was passed into
+the function (PDF, Google Slides, Google Docs, etc.)
+@return String type of file
+*/
+export function extractFileOrURLType(file){
+
+}
+
+export default {generateRandomCode, extractFileOrURLType};
