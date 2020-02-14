d33bdf4c9257fb847a0381c9f9d5806617027ff0
Johnny Luong <thejohnnyluong@gmail.com>
2020-02-14 04:41:50 -0800

    Created Join page & added to routes in App.js

--- workshop-facilitator/src/App.js
+++ workshop-facilitator/src/App.js
@@ -1,19 +1,21 @@
-import React from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Host from "./routes/Host";
 import Create from "./routes/Create";
 import Home from "./routes/Home";
-import logo from './logo.svg';
-import './App.css';
+import Join from "./routes/Join";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div>
       <Router>
-        <Route exact path="/" component={Home}/>
-        <Route exact path="/home" component={Home}/>
-        <Route exact path="/host" component={Host}/>
-        <Route exact path="/create" component={Create}/>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/home" component={Home} />
+        <Route exact path="/host" component={Host} />
+        <Route exact path="/create" component={Create} />
+        <Route exact path="/join" component={Join} />
       </Router>
     </div>
   );
