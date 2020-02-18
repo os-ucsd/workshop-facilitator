3e18e70cdd976a2f259dfed7d8b2c0553ae3798d
DustinLin <dustinylin@gmail.com>
2020-02-18 15:18:39 -0800

    basic structure of string-file-parser

--- workshop-facilitator/src/utils.js
+++ workshop-facilitator/src/utils.js
@@ -15,7 +15,42 @@ export function generateRandomCode(){
 the function (PDF, Google Slides, Google Docs, etc.)
 @return String type of file
 */
+
+/*
+Thinking of
+-.PDF (website and file): https://www.orimi.com/pdf-test.pdf, or pdf-test.pdf
+- google Slides: docs.google.com/presentation/ , seems like you can't download them
+- powerpoint: end in .ppt extension , online hard to distinguish depending on where you store it, (drive, dropbox...)
+- google Docs: docs.google.com/document/ , can only downlaod as pdf or docx
+- microsoft Docs: .docx exetension
+- plain text: .txt (not sure if anyone actaully uses this in a workshop)
+
+*/
 export function extractFileOrURLType(file){
+    //assume ppl won't name file "https://" or "http://", if that's even allowed
+    if(file.indexOf("https://") > 0 || file.indexOf("http://") > 0){
+        if(file.indexOf(".pdf") > 0){
+            return "pdf";
+        }else if(file.indexOf("presentation" > 0)){
+            return "slides";
+        }else if(file.indexOf("document") > 0){
+            return "docs";
+        }else{
+            return "unknown";
+        }
+    }else{
+        if(file.indexOf(".ppt") > 0){
+            return "powerPoint";
+        }else if(file.indexOf(".docx") > 0){
+            return "wordDoc";
+        }else if(file.indexOf(".pdf") > 0){
+            return "pdf";
+        }else if(file.indexOf(".txt") > 0){
+            return "text";
+        }else{
+            return "unknown";
+        }
+    }
 
 }
 
