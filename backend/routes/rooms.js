8e62ddc3c1a36dabf2cf3ab3d13ca3e918ccf463
DustinLin <dustinylin@gmail.com>
2020-02-27 17:55:50 -0800

    success upload createData in room, not success in redirect

--- backend/routes/rooms.js
+++ backend/routes/rooms.js
@@ -3,6 +3,9 @@ Holds the API routes having to deal with the workshop rooms
 */
 const express = require('express');
 const router = express.Router();
+const WorkshopRoom = require("../models/workshop_room.model.js"); //added to be able to use Schema
+
+
 
 /*
 @route GET /rooms/
@@ -10,14 +13,65 @@ const router = express.Router();
 */
 router.route('/').get((req, res) => {
     // database query
+
+
+    /* work in progress for get all rooms
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    console.log("fetching all rooms...");
+
+    WorkshopRoom.find()
+        .then(rooms => res.json(rooms))
+        .catch(err => res.status(400).json(err))
+    */
 })
+
 
 /*
 @route POST /rooms/create
 @desc Creates a new workshop room
 */
 router.route('/create').post((req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+
+
+    /* for testing
+    console.log("in /create Route");
+    console.log("Here is the req,body below");
+    console.log(req.body);
+    */
+
     // database query
+    //use req.body info to create new room, save it
+
+    const newRoom = new WorkshopRoom({
+        hostCode: req.body.joinCodeHost,
+        name: req.body.nameForm,
+        wsTitle: req.body.wsTitleForm,
+        wsDescript: req.body.wsDescripForm,
+        joinCode: req.body.joinCodeHost,
+        resources: {
+            name: "",
+            link: ""
+        },
+        questions: {
+            title: "",
+            description: ""
+        },
+        attendees: [],
+        wfclickers: [{
+            clicker: "",
+            answers: [],
+            correct: ""
+        }],
+        feedback: ""
+    });
+
+    newRoom.save()
+        .then(() => res.redirect('/host')) //not redirecting properly
+        .catch(err => console.log(err));
+        //.catch(err => err.status(400).json(err));
+
 })
 
 /*
