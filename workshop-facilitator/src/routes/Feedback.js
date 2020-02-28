16017687c7c8793d473a31bc5c92bcfcc6f2d0a2
Steven <stevenharris434@gmail.com>
2020-02-27 18:58:58 -0800

    merged with master

---
+++ workshop-facilitator/src/routes/Feedback.js
@@ -0,0 +1,75 @@
+import React from "react";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import { BrowserRouter as Router, Link } from "react-router-dom";
+
+import "../styles/Feedback.css";
+
+class Feedback extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      email: "",
+      roomCode: "",
+      valid: false
+    };
+  }
+
+  handleChange = (e) => {
+    this.setState({
+        [e.target.id]: e.target.value
+    })
+}
+
+  submitForm = e => {
+    e.preventDefault();
+    console.log(e.target);
+    this.setState({
+      valid: true //Handle code validation here
+    });
+
+  };
+
+  render() {
+    return (
+      <div className="">
+        <Link to="/join" className="back">
+          <Button width="100%" variant="contained" color="primary">
+            Back
+          </Button>
+        </Link>
+        <div className="vertical-center">
+            <h1>Enter your name and email if you would like feedback after the workshop!</h1>
+            <form noValidate autoComplete="off" onSubmit={this.submitForm} class="FollowupForm">
+              <TextField
+                required
+                id="name"
+                label="Name"
+                defaultValue=""
+              />
+              <br />
+              <br />
+              <br />
+              <TextField
+                required
+                id="email"
+                label="e-mail"
+                defaultValue=""
+              />
+              <br />
+              <br />
+              <br />
+              <Link to="/user" className="userLink" color="secondary">
+                <Button width="100%" variant="contained" color="secondary">
+                  Join
+                </Button>
+              </Link>
+            </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Feedback;
